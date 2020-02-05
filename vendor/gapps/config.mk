247a31b5aac694532aa84dbd7d97cd0a0170cee7
rahul9999xda <rahul9999xda@gmail.com>
2020-02-05 21:16:25 +0530

    X00TD: Debloated version of vendor/gapps/config.mk
    How to use -
    
    rm -rf vendor/gapps/config.mk && wget -P vendor/gapps/ https://raw.githubusercontent.com/x00t-los/pixy/master/vendor/gapps/config.mk

---
+++ vendor/gapps/config.mk
@@ -0,0 +1,83 @@
+ifeq ($(TARGET_GAPPS_ARCH),)
+$(error "GAPPS: TARGET_GAPPS_ARCH is undefined")
+endif
+
+ifneq ($(TARGET_GAPPS_ARCH),arm)
+ifneq ($(TARGET_GAPPS_ARCH),arm64)
+$(error "GAPPS: Only arm and arm64 are allowed")
+endif
+endif
+
+$(call inherit-product, vendor/gapps/common-blobs.mk)
+
+# Include package overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/gapps/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/gapps/overlay/common/
+
+ifeq ($(IS_PHONE),true)
+PRODUCT_PACKAGES += \
+    com.google.android.dialer.support
+endif
+
+# app
+ifeq ($(IS_PHONE),true)
+PRODUCT_PACKAGES += \
+    PrebuiltBugle
+endif
+
+ifeq ($(TARGET_INCLUDE_STOCK_ARCORE),true)
+PRODUCT_PACKAGES += \
+    arcore
+endif
+
+# System app
+PRODUCT_PACKAGES += \
+    GoogleExtShared \
+    GooglePrintRecommendationService
+
+# System priv-app
+PRODUCT_PACKAGES += \
+    GoogleDocumentsUIPrebuilt \
+    GoogleExtServicesPrebuilt \
+    GooglePermissionControllerPrebuilt
+
+# Product app
+PRODUCT_PACKAGES += \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Chrome \
+    Gmail \
+    GoogleContacts \
+    GoogleContactsSyncAdapter \
+    LatinIMEGooglePrebuilt \
+    LocationHistoryPrebuilt \
+    MarkupGoogle \
+    PrebuiltDeskClockGoogle \
+    SoundPickerPrebuilt \
+    talkback \
+    WebViewGoogle
+
+# Product priv-app
+PRODUCT_PACKAGES += \
+    AndroidMigratePrebuilt \
+    CarrierServices \
+    ConfigUpdater \
+    ConnMetrics \
+    GoogleDialer \
+    GoogleFeedback \
+    GoogleOneTimeInitializer \
+    GooglePartnerSetup \
+    GoogleServicesFramework \
+    Phonesky \
+    PixelSetupWizard \
+    PrebuiltGmsCoreQt \
+    SetupWizardPrebuilt \
+    Velvet \
+    WallpaperPickerGoogleRelease \
+    WellbeingPrebuilt
+
+ifeq ($(IS_PHONE),true)
+PRODUCT_PACKAGES += \
+    GoogleDialer
+endif
