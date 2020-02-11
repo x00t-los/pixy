e54166cb89c1cbb8dffdaf03db8ab9880c1a4d97
rahul9999xda <rahul9999xda@gmail.com>
2020-02-11 20:21:39 +0530

    Add vendor packages config

---
+++ vendor/aosip/packages.mk
@@ -0,0 +1,53 @@
+# Required packages
+PRODUCT_PACKAGES += \
+    OmniRecord \
+    OmniStyle \
+    ThemePicker \
+    AOSiPOverlayStub
+
+ifeq ($(DERP_BUILD_ZIP_TYPE), VANILLA)
+PRODUCT_PACKAGES += \
+    Gallery2 \
+    LatinIME \
+    messaging
+endif
+
+ifeq ($(filter-out Official CI Private, $(DERP_BUILDTYPE)),)
+    PRODUCT_PACKAGES += \
+        OpenDelta
+endif
+
+# Extra tools in AOSiP
+PRODUCT_PACKAGES += \
+    7z \
+    awk \
+    bash \
+    bzip2 \
+    curl \
+    getcap \
+    htop \
+    lib7z \
+    libsepol \
+    nano \
+    pigz \
+    powertop \
+    setcap \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
+
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librsjni
